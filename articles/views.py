38792c449008ce7016fc1fc4f29bb49fa125069d
Faniry Aomin <fanirintsoaharijaona@gmail.com>
2022-06-17 04:52:51 +0300

    authorisations for editing an article

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import ListView,DetailView
 from django.views.generic import UpdateView,DeleteView,CreateView
 from django.urls import reverse_lazy
@@ -6,25 +7,40 @@ from.models import Article
 
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model           =   Article
     template_name   =   'article_list.html'  
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(UserPassesTestMixin,LoginRequiredMixin,DeleteView):
     model           =   Article
     template_name   =   'article_delete.html'
     success_url     =   reverse_lazy('article_list')
 
-class   ArticleDetailView(DetailView):
+    def  test_func(self):
+        obj         =   self.get_object()
+        return obj.authors==self.request.user
+
+
+class   ArticleDetailView(LoginRequiredMixin,DetailView):
     model           =   Article
     template_name   =   'article_detail.html'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(UserPassesTestMixin,LoginRequiredMixin,UpdateView):
     model           =   Article
     fields          =   ('title','body',)
     template_name   =   'article_edit.html'
 
-class ArticleCreateView(CreateView):
+    def test_func(self):
+        obj         =   self.get_object()
+        return obj.authors==self.request.user
+
+
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model           =   Article
     template_name   =   'article_create.html'
-    fields          =   ('title','authors','body',)
+    fields          =   ('title','body',)
+
+    def form_valid(self,form):
+        form.instance.authors  =  self.request.user  
+        return super().form_valid(form) 
